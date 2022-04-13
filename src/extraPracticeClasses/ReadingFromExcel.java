ba1bc79097f573e6f3a6555244a5739985890725
moe <moekhan343@gmail.com>
2022-04-12 23:24:12 -0500

    Adding comments for the Excel practice

--- src/extraPracticeClasses/ReadingFromExcel.java
+++ src/extraPracticeClasses/ReadingFromExcel.java
@@ -14,7 +14,7 @@ public class ReadingFromExcel {
     public static void main(String[] arg){
         //Simple test to see that the value we got from the file matches what we expect
         //https://support.spatialkey.com/spatialkey-sample-csv-data/ << practice website to download csv files from
-        getRowsFromFile().get(6).get(5).equals("530.5 PC USE PERSONAL ID INFO");
+        getRowsFromFile();
     }
 
     public static ArrayList<List<String>> getRowsFromFile(){
@@ -28,8 +28,10 @@ public class ReadingFromExcel {
             XSSFSheet sheet = workbook.getSheetAt(0);
 
             for (int i = 0; i <= sheet.getRow(0).getPhysicalNumberOfCells(); i++) {
+                //for each row create a new list
                 List<String> tempList = new ArrayList<>();
                 for (int j = 0; j < sheet.getLastRowNum(); j++) {
+                    //for each row iterate through the columns and create a list
                     tempList.add(sheet.getRow(i).getCell(j).getStringCellValue());
                 }
                 records.add(i, tempList);
